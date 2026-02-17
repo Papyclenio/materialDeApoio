eba3746f808dc171af2a9e865127636dcc3366d1
Makumbani Eduardo Pedro <161555173+Papyclenio@users.noreply.github.com>
2026-02-18 00:41:26 +0100

    Add files via upload

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lst_path = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_remove = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lst_path
+            // 
+            this.lst_path.FormattingEnabled = true;
+            this.lst_path.Location = new System.Drawing.Point(12, 12);
+            this.lst_path.Name = "lst_path";
+            this.lst_path.Size = new System.Drawing.Size(124, 225);
+            this.lst_path.TabIndex = 0;
+            this.lst_path.SelectedIndexChanged += new System.EventHandler(this.lst_path_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Informe o caminho da imagem:\r\n";
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(154, 39);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(242, 20);
+            this.txt_path.TabIndex = 2;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(275, 65);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(121, 23);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "Abrir imagem";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.Location = new System.Drawing.Point(154, 65);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(111, 23);
+            this.btn_remove.TabIndex = 4;
+            this.btn_remove.Text = "Remover Path";
+            this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(142, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 143);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 248);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_remove);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lst_path);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lst_path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_remove;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
